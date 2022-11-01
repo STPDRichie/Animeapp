e0d6a4332b2a6026bbcf523f4f28ab7f7154f6cd
Nikolay Karpov <nkarpov@netangels.ru>
2022-11-01 12:26:44 +0500

    добавил кейсы для fetchPopular в reducers/homePage

--- src/reducers/homePage.js
+++ src/reducers/homePage.js
@@ -1,14 +1,38 @@
+import {
+    FETCH_POPULAR_IN_PROGRESS,
+    FETCH_POPULAR_SUCCESSFULLY,
+    FETCH_POPULAR_WITH_ERRORS,
+} from '../actions/homePage/actionTypes';
+
 export default () => {
     const defaultState = {
+        popular: null,
         popularInProgress: false,
     }
 
     return (state = defaultState, action) => {
         switch (action.type) {
-            case 'get_number':
+            case FETCH_POPULAR_SUCCESSFULLY: {
+                console.log(action.data);
                 return {
                     ...state,
+                    popular: action.data,
+                    popularInProgress: false,
                 };
+            }
+            case FETCH_POPULAR_IN_PROGRESS: {
+                return {
+                    ...state,
+                    popular: null,
+                    popularInProgress: true,
+                };
+            }
+            case FETCH_POPULAR_WITH_ERRORS: {
+                return {
+                    ...state,
+                    popularInProgress: false,
+                };
+            }
             default:
                 return state;
         }
