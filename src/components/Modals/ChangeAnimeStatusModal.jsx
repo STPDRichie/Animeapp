7d670c55253d3dceb888f20c01ff8da5b9e134bc
Nikolay Karpov <nkarpov@netangels.ru>
2022-11-13 00:00:49 +0500

    добавил в ChangeAnimeStatusModal Button, задал стили header'у. немного изменил стили Modal и Button

--- src/components/Modals/ChangeAnimeStatusModal.jsx
+++ src/components/Modals/ChangeAnimeStatusModal.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
 import Modal from '../General/Modal/Modal';
+import Button from '../General/Button/Button';
 
 import { animeCardFormat } from '../AnimeCard/constants';
 
 function ChangeAnimeStatusModal(props) {
     const { animeCard } = props;
+    const { title, coverImage } = animeCard;
 
     return (
         <Modal modifier="change-anime-status-modal">
             <div
-                className="anime__banner"
+                className="change-anime-status__header modal-content"
                 style={{ backgroundImage: `url(${animeCard.bannerImage})` }}
-            />
+            >
+                <div className="change-anime-status__info">
+                    <div className="change-anime-status__image-wrapper">
+                        <img src={coverImage.large} />
+                    </div>
+                    <div className="change-anime-status__title">
+                        {title.userPreferred}
+                    </div>
+                </div>
+                <div className="change-anime-status__buttons">
+                    <Button>Save</Button>
+                </div>
+            </div>
             <div className="modal-content"></div>
         </Modal>
     );
