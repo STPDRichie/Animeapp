2c33555673d9096cee27f903987fbfada72155d2
STPDRichie <nikolaj.karpov.2002@mail.ru>
2022-11-19 18:35:55 +0500

    убрал пробел в конце OAUTH_URL

--- api/anilist_api/__init__.py
+++ api/anilist_api/__init__.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import get_jwt
 
 BASE_URL = 'https://graphql.anilist.co'
 OAUTH_URL = 'https://anilist.co/api/v2/oauth/authorize?client_id=9997' \
-            '&response_type=token '
+            '&response_type=token'
 
 
 def make_request(query, variables=None):
